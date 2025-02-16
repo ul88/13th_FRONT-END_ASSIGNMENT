77d12089ec971a06b799bc4da4c9f3a1f6fbd34b
iubns <iubns@naver.com>
2025-02-17 00:19:18 +0900

    update: junho week2 build

--- junho/week2/next.config.ts
+++ junho/week2/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: "export",
+  assetPrefix: "/13th_FRONT-END_ASSIGNMENT/junho/week2/out",
+}
 
-export default nextConfig;
+export default nextConfig
